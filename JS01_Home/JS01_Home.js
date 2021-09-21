bb533a0e94c87265aac7a28be5ce5711d0673bce
Do Minh Hieu <minhhieudo.it@gmail.com>
2021-09-21 17:40:35 +0700

    Bai1 Hello JS

---
+++ JS01_Home/JS01_Home.js
@@ -0,0 +1,19 @@
+//=========================JAVASCRIPT===============================
+// int : số nguyên nguyên 
+// float :1.2 
+//string : chuỗi
+// boolean : true - false : 
+// object :
+var a  ; //=> tự hiểu răng a bây giờ là kiểu số nguyên
+a = 1 ;
+// a là kiểu số nguyên 
+a =  "Javascript";
+//cách khai báo biến 
+var a =1 , b = 2 ,c =3 ;
+// var d = 3 ;
+// var f = 4 ;  
+// shorthand 
+var d = 3 , f = 4;
+//alert("kiểu dữ liệu của a ="+typeof(a));
+var kq = "kết quả a ="+a+"kết quả b ="+b+"kết quả c="+c;
+alert(kq);
