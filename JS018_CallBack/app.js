fb946b65740d84bcf237ecc8831a8cf6e7f45c76
mizhhieudo <minhhieudo.it@gmail.com>
2022-04-08 04:15:46 +0700

    git-example-callback-callbackhell

--- JS018_CallBack/app.js
+++ JS018_CallBack/app.js
@@ -1,48 +1,51 @@
-// Task A : start 
-// task b : lấy dữ liệu trong 2 s 
-// task c  : hiển thị dữ liệu vừa lấy được 
-let data = [{
-    username:'mizhhieudo.it',
-    gitRepo : 'learnJS'
-}]
-function TaskA(){
-    console.log('start...');
-}
-// task B get username from source data
-function TaskB(){
+// vấn đề cũ
+console.log('Befor');
+const user = getUser(1);
+console.log(user);
+console.log('after');
+
+
+// function get info user 
+function getUser(id) {
     setTimeout(() => {
-       data.map((x)=>{
-           return x.username
-       }) 
+        console.log('Reading a user from a database......');
+        return {id : id , gitHubUsername : 'Test'}
+    }, 2000);
+    return 1 
+}
+// =====> vấn Problem need slove : how to get info user 
+//== level 1 : callbacks
+function getUserUseCallBack(id,callback) {
+    setTimeout(() => {
+        console.log('Reading a user from a database......');
+        // dữ liệu sẽ không trực tiếp trả về 2 giây trong điểm gọi , mà 2 sau 2 giây nó sẽ chờ để thực thi 
+        // chuyển kết quả đó cho 1 hàm khác
+        callback({id : id , gitHubUsername : 'Test'}) 
+    }, 2000);
+    return 1 
+}
+console.log("==========Call Back===========");
+console.log('Befor callback...');
+getUserUseCallBack(1,(user)=>{
+    console.log('data sent here :'+user.id + "git name :"+user.gitHubUsername);
+    /// sử dụng getRepo 
+    getRepository(user.gitHubUsername,(repoGit)=>{
+        console.log("git repository",repoGit);
+        getCommitUser(repoGit[0],(dataCommits)=>{
+            console.log("commit in repo"+dataCommits);
+            // task =>>> Call Hell 
+        })
+    })
+});
+console.log('after Call back');
+// Exp : chuyển thành bất đồng bộ 
+function getRepository(username,callback) {  // chuyển thành bất đồng bộ -> settimeout
+    setTimeout(() => {
+        callback(['repo1','repo2'])
+    }, 2000); 
+}
+function getCommitUser(repo,callback) {
+    setTimeout(() => {
+        callback(["commit1","commit2"])
     }, 2000);
 }
-function TaskC(params) {
-    console.log(params);
-    console.log('End Process..........');
-
-}
-// don't use callback . ..
-// TaskA();
-// let result = TaskB(); // underfind 
-// TaskC(result);
-// how to implement this process ? 
-// => CallBack 
-function TaskX(){
-    console.log('start...');
-}
-// task B get username from source data
-function TaskY(callback){
-    setTimeout(() => {
-       let resultName =  data.map((x)=>{
-           return x.username + x.gitRepo
-       }) ;
-       callback(resultName) ;
-    }, 2000);
-}
-function TaskZ(params) {
-    console.log(params);
-    console.log('End Process..........');
-
-}
-TaskX();
-TaskY(TaskZ); // use call back
